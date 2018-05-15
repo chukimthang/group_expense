55b4c0762a08c52e2f348f5708b0470d6f27d9fc
Chu Kim Thang <chukimthang94@gmail.com>
2018-05-15 23:44:22 +0700

    Update mysql_options utf8 migration

--- db/migrate/20180409165112_create_user_groups.rb
+++ db/migrate/20180409165112_create_user_groups.rb
@@ -1,6 +1,6 @@
 class CreateUserGroups < ActiveRecord::Migration[5.1]
   def change
-    create_table :user_groups, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    create_table :user_groups, mysql_options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
       t.integer :user_id, default: 0
       t.integer :group_id, default: 0
       t.integer :user_admin_group_id, default: 0
